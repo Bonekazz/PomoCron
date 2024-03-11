83ab93f443f0dee0926cba6bfac2c17b9c434c6c
Bonekazz <juracirdantas@gmail.com>
2024-03-11 17:24:50 -0300

    [feat]: add fixed time copy

--- src/Classes/Cronos.ts
+++ src/Classes/Cronos.ts
@@ -3,7 +3,7 @@
 // -- Avançar e retomar blocos
 // -- selecionar um bloco para ser o atual
 
-import { CronState } from "../Types/BlockTypes";
+import { CronState, ITime } from "../Types/BlockTypes";
 import CronRenderer from "./CronRenderer";
 import Demeter from "./Demeter";
 import TimeBlock from "./TimeBlock";
@@ -17,7 +17,8 @@ export default class Cronos
 {
     private _currentDemeter: Demeter | null = null;
     private _currentBlock: TimeBlock | null = null;                         // A copy of the Demeter current block
-    private _currentState: CronState | null = null;       // Running, Paused, Finished;
+    private _fixedCurrentTime: ITime | null = null;                         // A copy of the Demeter current block time      
+    private _currentState: CronState | null = null;                         // Running, Paused, Finished;
 
     private _blockInterval: number | null = null;
 
@@ -57,24 +58,28 @@ export default class Cronos
                 return;
             }
 
-        //    if (this._currentState === CronState.FINISHED) {
-        //        if (this._blockInterval === null) return;
-        //        clearInterval(this._blockInterval);
-        //        this.advanceBlock();
-        //        this.runCurrentBlock();
-        //        return;
-        //    }
+            if (this._currentState === CronState.FINISHED) {
+                if (this._blockInterval === null) return;
+                clearInterval(this._blockInterval);
+                this.advanceBlock();
+                this.runCurrentBlock();
+                return;
+            }
             
             this.burnCurrentBlock();
-            this._renderer.renderBlockTime(this._currentBlock._config.time);
+            if (this._fixedCurrentTime === null) throw new Error("Fixed Current Time is null");
+            this._renderer.renderBlockTime(this._currentBlock._config.time, this._fixedCurrentTime);
 
         }, 1000);
     };
 
     burnCurrentBlock() {
         if (this._currentBlock === null) throw new Error("there is no block setted");
-        
-        this._currentBlock._config.time.seconds -= 1;
+
+        if (this._currentBlock._config.time.hours === 0 && this._currentBlock._config.time.minutes === 0 && this._currentBlock._config.time.seconds === 0) {
+            this._currentState = CronState.FINISHED;
+            return;
+        }
         if (this._currentBlock._config.time.seconds <= 0) {
             this._currentBlock._config.time.seconds = 59;
             this._currentBlock._config.time.minutes -= 1;
@@ -86,38 +91,58 @@ export default class Cronos
             this._currentBlock._config.time.seconds = 59;
         }
 
-        if (this._currentBlock._config.time.hours === 0 && this._currentBlock._config.time.minutes === 0 && this._currentBlock._config.time.seconds === 0) {
-            this._currentState = CronState.FINISHED;
-        }
-        
-        console.log(this._currentBlock._config.time.hours, this._currentBlock._config.time.minutes, this._currentBlock._config.time.seconds);
+        this._currentBlock._config.time.seconds -= 1;
+
+        console.log(`CURRENT -> ${this._currentBlock._config.time.hours}:${this._currentBlock._config.time.minutes}:${this._currentBlock._config.time.seconds} --------- DEFAULT => ${this._fixedCurrentTime?.hours}:${this._fixedCurrentTime?.minutes}:${this._fixedCurrentTime?.seconds}`);
+    }
+
+    pauseCurrentBlock() {
+        if (this._blockInterval === null) throw new Error("there is no interval");
+        if (this._currentState === CronState.PAUSED) { console.log("block already paused"); return; };
+        this._currentState = CronState.PAUSED;
+        clearInterval(this._blockInterval);
+        console.log("block paused");
     }
     
     resetCurrentBlock() {
         if (this._currentDemeter === null) throw new Error("Nao há nenhum Demeter selecionado");
         if (this._currentBlock === null) throw new Error("Demeter atual está vazio");
-    
-        this._currentBlock._config.time = this._currentDemeter.getCurrentBlock()._config.time;
+        if(this._currentState === CronState.RUNNING) throw new Error("Cronos em andamento");
+            if (this._fixedCurrentTime === null) throw new Error("Fixed Current Time is null");
+        
+        this.pauseCurrentBlock();
+        this._currentBlock._config.time = this._fixedCurrentTime;
+
+        // RENDER BLOCK RESET
+        this._renderer.renderBlockTime(this._currentBlock._config.time, this._fixedCurrentTime);
     }
     
-    setCurrentBlock(block: TimeBlock) {
+    setCurrentBlock(block: TimeBlock | null) {
+        if (this._currentDemeter === null) throw new Error("There is no Demeter setted");
+        if (block === null) throw new Error("Não há mais blocos para rodar");
+
         this._currentState = CronState.PAUSED;
-        this._currentBlock = {...block};
-        //this._renderer.renderBlockConfig(this._currentBlock); --> will render the title and the time;
+        this._currentBlock = block;
+        this._fixedCurrentTime = JSON.parse(JSON.stringify(block._config.time)) as ITime;
+        this._renderer.renderBlockTime(this._currentBlock._config.time, this._fixedCurrentTime);
+        //this._renderer.renderBlockConfig(this._currentBlock); --> will render the title, time and progress bar;
     };
 
     backBlock(){
         if (this._currentDemeter === null) throw new Error("Nao há nenhum Demeter selecionado");
-        this.setCurrentBlock(this._currentDemeter.getPreviousBlock());
+        this.setCurrentBlock(JSON.parse(JSON.stringify(this._currentDemeter.getPreviousBlock())));
     };
     advanceBlock() {
+        // what if there is no next block?
         if (this._currentDemeter === null) throw new Error("Nao há nenhum Demeter selecionado");
-        this.setCurrentBlock(this._currentDemeter.getNextBlock());
+        if (this._blockInterval === null) throw new Error("No interval found");
+
+        this.setCurrentBlock(JSON.parse(JSON.stringify(this._currentDemeter.getNextBlock())));
     };
 
     setDemeter(demeter: Demeter) {
         this._currentDemeter = demeter;
-        this.setCurrentBlock(this._currentDemeter.getCurrentBlock());
+        this.setCurrentBlock(JSON.parse(JSON.stringify(this._currentDemeter.getCurrentBlock())));
         this._renderer.renderDemeter(this._currentDemeter);
     }
 
