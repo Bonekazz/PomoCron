8de77005fb567bff1b0b5b1181053540614ab478
Bonekazz <juracirdantas@gmail.com>
2024-03-11 17:22:51 -0300

    [feat]: add lucide icons

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import CronRenderer from "./Classes/CronRenderer";
 import Cronos from "./Classes/Cronos";
 import Demeter from "./Classes/Demeter";
 
+import { createIcons, Menu } from "lucide";
+
+
+// APP
+
 const renderer = new CronRenderer();      // creayed by the application
 const cronos = new Cronos(renderer);     // creayed by the application
 const demeter1 = new Demeter();          // created by the user
@@ -10,3 +15,12 @@ const demeter1 = new Demeter();          // created by the user
 renderer.setCronos(cronos);
 
 cronos.setDemeter(demeter1);
+
+
+// ICONS 
+
+createIcons({
+    icons: {
+        Menu
+    }
+});
